f15f3372685acab4f88cf63d62e1d71f62cb34c2
Rasmus Hansen <32864091+RH-FullStack@users.noreply.github.com>
2024-06-24 09:57:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const forecastElement = document.getElementById('weather-forecast');
+
+    // Simuler en prognose for sne
+    const snowForecast = "Desværre, ingen sne i dag. Nyd sommeren!";
+
+    forecastElement.textContent = snowForecast;
+});
